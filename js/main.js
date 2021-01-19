2ac569ac0b46415414aa565b039ad8dae486142d
ArkadiiVodolazskyi <jswift168@gmail.com>
2021-01-19 22:41:13 +0200

    .

--- js/main.js
+++ js/main.js
@@ -1,7 +1,25 @@
 window.addEventListener("load", () => {
   // Hide page content while loading
   document.body.classList.remove("loading");
-  console.log("Window loaded.");
+
+  // Initialize GMaps
+  (function () {
+    function initMap() {
+      const coordinates = { lat: 46.36541074235588, lng: 30.716028340841838 };
+      const map = new google.maps.Map(document.querySelector(".map"), {
+        center: coordinates,
+        zoom: 17,
+        disableDefaultUI: false,
+        scrollwheel: true,
+      });
+      const marker = new google.maps.Marker({
+        position: coordinates,
+        map: map,
+      });
+    }
+
+    const key = "AIzaSyAUP-_EwsSSdNItxU_gnpAfsC_rdGv_qsg";
+  })();
 
   // Use .img-svg on image to remove it with svg version
   (function () {
@@ -45,24 +63,97 @@ window.addEventListener("load", () => {
     });
     wow.init();
   })();
-
-  // Initialize GMaps
-  (function () {
-    function initMap() {
-      const coordinates = { lat: 46.480810812052994, lng: 30.730663527352412 };
-      const map = new google.maps.Map(
-        document.querySelector(".footer-main__map"),
-        {
-          center: coordinates,
-          zoom: 17,
-          disableDefaultUI: false,
-          scrollwheel: true,
-        },
-      );
-      const marker = new google.maps.Marker({
-        position: coordinates,
-        map: map,
-      });
-    }
-  })();
 });
+
+// ! remove to page load while code is fully ready
+
+// + page-vacancies - expand
+// (function () {
+//   const vacancies = [...document.querySelectorAll(".vacancy")];
+//   const vacExpands = [...document.querySelectorAll(".expand")];
+
+//   // Default
+//   vacancies[0].classList.add("active");
+
+//   for (let i = 0; i < vacExpands.length; i++) {
+//     vacExpands[i].addEventListener("click", () => {
+//       if (vacancies[i].classList.contains("active")) {
+//         vacancies[i].classList.remove("active");
+//       } else {
+//         for (let j = 0; j < vacancies.length; j++) {
+//           vacancies[j].classList.remove("active");
+//         }
+
+//         vacancies[i].classList.add("active");
+//       }
+//     });
+//   }
+// })();
+
+// + page-about - master slider
+// (function () {
+//   const masterPrev = document.querySelector(".master_prev");
+//   const masterNext = document.querySelector(".master_next");
+
+//   const masterNames = [...document.querySelectorAll(".master_name .name")];
+//   const masterImgs = [...document.querySelectorAll(".master_img img")];
+
+//   // Default
+//   masterIndex = 0;
+//   masterNames[masterIndex].classList.add("active");
+//   masterImgs[masterIndex].classList.add("active");
+//   masterPrev.classList.remove("active");
+//   masterNext.classList.add("active");
+
+//   function masterChange(masterIndex) {
+//     if (masterIndex < 0) {
+//       masterIndex = 0;
+//       masterPrev.classList.remove("active");
+//     } else if (masterIndex > masterNames.length - 1) {
+//       masterIndex = masterNames.length - 1;
+//       masterNext.classList.remove("active");
+//     } else {
+//       masterPrev.classList.add("active");
+//       masterNext.classList.add("active");
+//     }
+
+//     if (masterIndex - 1 < 0) {
+//       masterPrev.classList.remove("active");
+//     }
+//     if (masterIndex + 1 > masterNames.length - 1) {
+//       masterNext.classList.remove("active");
+//     }
+
+//     for (let i = 0; i < masterNames.length; i++) {
+//       masterNames[i].classList.remove("active");
+//       masterImgs[i].classList.remove("active");
+//     }
+
+//     masterNames[masterIndex].classList.add("active");
+//     masterImgs[masterIndex].classList.add("active");
+//   }
+
+//   masterPrev.addEventListener("click", () => {
+//     masterIndex--;
+
+//     masterChange(masterIndex);
+//   });
+
+//   masterNext.addEventListener("click", () => {
+//     masterIndex++;
+
+//     masterChange(masterIndex);
+//   });
+// })();
+
+// page-service_post - expand nav
+(function () {
+  const navPoints = [...document.querySelectorAll("aside .list > li")];
+
+  for (let i = 0; i < navPoints.length; i++) {
+    navPoints[i].addEventListener("click", (e) => {
+      e.preventDefault();
+      navPoints[i].classList.toggle("active");
+    });
+  }
+})();
