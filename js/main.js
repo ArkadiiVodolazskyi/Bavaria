7431e9ddcaf9d581865b907da15b58eae73b98fe
ArkadiiVodolazskyi <jswift168@gmail.com>
2021-01-23 22:21:27 +0200

    .

--- js/main.js
+++ js/main.js
@@ -2,6 +2,28 @@ window.addEventListener("load", () => {
   // Hide page content while loading
   document.body.classList.remove("loading");
 
+  // Global variables and functions
+  const overlay = document.getElementById("overlay");
+  const closeOverlay = document.getElementById("closeOverlay");
+
+  function openOverlay(element) {
+    overlay.classList.add("activeOverlay");
+    closeOverlay.classList.add("activeOverlay");
+    element.classList.add("activeOverlay");
+
+    closeOverlay.addEventListener("click", () => {
+      const activeOverlays = document.querySelectorAll(".activeOverlay");
+      activeOverlays.forEach((activeOverlay) => {
+        activeOverlay.classList.remove("activeOverlay");
+      });
+    });
+    overlay.addEventListener("click", () => {
+      activeOverlays.forEach((activeOverlay) => {
+        activeOverlay.classList.remove("activeOverlay");
+      });
+    });
+  }
+
   // Initialize GMaps
   (function () {
     function initMap() {
@@ -69,15 +91,8 @@ window.addEventListener("load", () => {
     const hamb = document.querySelector(".hamb");
     const expandedMenu = document.querySelector("#expandedMenu");
     if (expandedMenu) {
-      const closeExpanded = document.querySelector(
-        "#expandedMenu .closeOverlay",
-      );
-
       hamb.addEventListener("click", () => {
-        expandedMenu.classList.add("expanded");
-      });
-      closeExpanded.addEventListener("click", () => {
-        expandedMenu.classList.remove("expanded");
+        openOverlay(expandedMenu);
       });
     }
   })();
@@ -130,9 +145,6 @@ window.addEventListener("load", () => {
 
     if (vacancies.length) {
       const vacExpands = [...document.querySelectorAll(".expand")];
-
-      // Default
-      vacancies[0].classList.add("active");
 
       for (let i = 0; i < vacExpands.length; i++) {
         vacExpands[i].addEventListener("click", () => {
@@ -222,28 +234,72 @@ window.addEventListener("load", () => {
     }
   })();
 
+  // page-service_post - services slider
+  (function () {
+    const serviceTape = document.querySelector(".popular .cards");
+
+    if (serviceTape) {
+      const serviceCards = [...serviceTape.querySelectorAll(".card ")];
+      const servicePrevBtn = document.querySelector(".popular .master_prev");
+      const serviceNextBtn = document.querySelector(".popular .master_next");
+
+      // Default
+      let currentTransf = 0;
+      const transDif = serviceCards[0].offsetWidth;
+      let firstActiveIndex = 0;
+      let lastActiveIndex = 3;
+      for (let i = firstActiveIndex; i <= lastActiveIndex; i++) {
+        serviceCards[i].classList.add("active");
+      }
+
+      servicePrevBtn.addEventListener("click", () => {
+        firstActiveIndex--;
+        if (firstActiveIndex < 0) {
+          firstActiveIndex = 0;
+        } else {
+          serviceCards[firstActiveIndex].classList.add("active");
+          currentTransf = currentTransf + transDif;
+          serviceTape.style.transform = `matrix(1, 0, 0, 1, ${currentTransf}), -50%`;
+          serviceCards[lastActiveIndex].classList.remove("active");
+          lastActiveIndex--;
+        }
+      });
+      serviceNextBtn.addEventListener("click", () => {
+        lastActiveIndex++;
+        if (lastActiveIndex > serviceCards.length - 1) {
+          lastActiveIndex = serviceCards.length - 1;
+        } else {
+          serviceCards[lastActiveIndex].classList.add("active");
+          currentTransf = currentTransf - transDif;
+          serviceTape.style.transform = `matrix(1, 0, 0, 1, ${currentTransf}), -50%`;
+          serviceCards[firstActiveIndex].classList.remove("active");
+          firstActiveIndex++;
+        }
+      });
+    }
+  })();
+
   // connect_form - open form, open overlay
   (function () {
     const connect_wrapper = document.getElementById("connect_wrapper");
 
     if (connect_wrapper) {
       const connectBtn = document.querySelectorAll(".openConnect");
-      const overlay = document.getElementById("overlay");
       const closeConnect = document.getElementById("closeConnect");
 
       connectBtn.forEach((btn) => {
         btn.addEventListener("click", () => {
-          overlay.classList.add("active");
-          connect_wrapper.classList.add("active");
+          overlay.classList.add("activeOverlay");
+          connect_wrapper.classList.add("activeOverlay");
         });
       });
       closeConnect.addEventListener("click", () => {
-        connect_wrapper.classList.remove("active");
-        overlay.classList.remove("active");
+        connect_wrapper.classList.remove("activeOverlay");
+        overlay.classList.remove("activeOverlay");
       });
       overlay.addEventListener("click", () => {
-        connect_wrapper.classList.remove("active");
-        overlay.classList.remove("active");
+        connect_wrapper.classList.remove("activeOverlay");
+        overlay.classList.remove("activeOverlay");
       });
     }
   })();
@@ -266,11 +322,8 @@ window.addEventListener("load", () => {
           const bigImages = [...clonedTape.querySelectorAll("img")];
 
           // Default
-          let currentTransf = 0;
-          const transDif = 170;
-          imageIndex = 0;
-          images[imageIndex].classList.add("active");
-          bigImages[imageIndex].classList.add("active");
+          images[0].classList.add("active");
+          bigImages[0].classList.add("active");
 
           // Change active on tape image click
           for (let i = 0; i < images.length; i++) {
@@ -285,31 +338,159 @@ window.addEventListener("load", () => {
           }
 
           // Move tape with imgs
+          let currentTransf = 0;
+          const transDif = 170;
+          let firstImgOnTape = 0;
+          let lastImgOnTape = 3;
+
           const tape_prev = galleries[i].querySelector(".tape_prev");
           const tape_next = galleries[i].querySelector(".tape_next");
 
           tape_prev.addEventListener("click", () => {
-            imageIndex--;
-            if (imageIndex < 0) {
-              imageIndex = 0;
+            firstImgOnTape--;
+            if (firstImgOnTape < 0) {
+              firstImgOnTape = 0;
             } else {
-              currentTransf += transDif;
-              tape.style.transform = `matrix(1, 0, 0, 1, 0, ${transDif})`;
+              lastImgOnTape--;
+              currentTransf = currentTransf + transDif;
+              tape.style.transform = `matrix(1, 0, 0, 1, 0, ${currentTransf})`;
             }
-            console.log(imageIndex, currentTransf);
           });
           tape_next.addEventListener("click", () => {
-            imageIndex++;
-            if (imageIndex > bigImages.length - 1) {
-              imageIndex = bigImages.length - 1;
+            lastImgOnTape++;
+            if (lastImgOnTape > bigImages.length - 1) {
+              lastImgOnTape = bigImages.length - 1;
             } else {
-              currentTransf -= transDif;
-              tape.style.transform = `matrix(1, 0, 0, 1, 0, ${transDif})`;
+              firstImgOnTape++;
+              currentTransf = currentTransf - transDif;
+              tape.style.transform = `matrix(1, 0, 0, 1, 0, ${currentTransf})`;
             }
-            console.log(imageIndex, currentTransf);
           });
         }
       }
+    }
+  })();
+
+  // page-about - open fillscreen video
+  (function () {
+    const video = document.getElementById("video");
+    const openVideo = document.getElementById("openVideo");
+
+    if (video && openVideo) {
+      openVideo.addEventListener("click", () => {
+        openOverlay();
+        video.classList.add("activeOverlay");
+      });
+    }
+  })();
+
+  // lightbox
+  (function () {
+    const lightboxes = [...document.querySelectorAll(".lightbox")];
+
+    if (lightboxes.length) {
+      for (let i = 0; i < lightboxes.length; i++) {
+        const imgs = lightboxes[i].querySelectorAll("img");
+
+        const clonedLightbox = document.createElement("div");
+        clonedLightbox.classList = "clonedLightbox";
+        const clonedTape = document.createElement("div");
+        clonedTape.classList = "clonedTape";
+
+        for (let k = 0; k < imgs.length; k++) {
+          let clonedImg = imgs[k].cloneNode(false);
+          clonedTape.appendChild(clonedImg);
+        }
+
+        const clonedPrevBtn = document.createElement("button");
+        clonedPrevBtn.textContent = "<";
+        clonedPrevBtn.classList = "clonedPrevBtn";
+        const clonedNextBtn = document.createElement("button");
+        clonedNextBtn.textContent = ">";
+        clonedNextBtn.classList = "clonedNextBtn";
+
+        clonedLightbox.appendChild(clonedTape);
+        clonedLightbox.appendChild(clonedPrevBtn);
+        clonedLightbox.appendChild(clonedNextBtn);
+        document.body.appendChild(clonedLightbox);
+
+        const clonedImgs = clonedLightbox.querySelectorAll("img");
+        clonedImgIndex = 0;
+
+        for (let j = 0; j < imgs.length; j++) {
+          imgs[j].addEventListener("click", () => {
+            openOverlay(clonedLightbox);
+
+            clonedImgs.forEach((image) => {
+              image.classList.remove("activeOverlay");
+            });
+            clonedImgs[j].classList.add("activeOverlay");
+            clonedImgIndex = j;
+          });
+        }
+
+        // Buttons
+        clonedPrevBtn.addEventListener("click", () => {
+          clonedImgIndex--;
+          if (clonedImgIndex < 0) {
+            clonedImgIndex = 0;
+          } else {
+            clonedImgs[clonedImgIndex + 1].classList = "";
+            clonedImgs[clonedImgIndex].classList = "activeOverlay";
+          }
+        });
+        clonedNextBtn.addEventListener("click", () => {
+          clonedImgIndex++;
+          if (clonedImgIndex > clonedImgs.length - 1) {
+            clonedImgIndex = clonedImgs.length - 1;
+          } else {
+            clonedImgs[clonedImgIndex - 1].classList = "";
+            clonedImgs[clonedImgIndex].classList = "activeOverlay";
+          }
+        });
+      }
+    }
+  })();
+
+  // page-service_post - change quote cards
+  (function () {
+    const quotes = [...document.querySelectorAll(".quote_card")];
+
+    if (quotes.length) {
+      const quotePrev = document.querySelector(".reviews .quote_prev");
+      const quoteNext = document.querySelector(".reviews .quote_next");
+
+      // Default
+      let quoteIndex = 0;
+      let perQuotes = 2;
+      for (let i = quoteIndex; i < perQuotes; i++) {
+        quotes[i].classList.add("active");
+      }
+
+      quotePrev.addEventListener("click", () => {
+        quoteIndex -= perQuotes;
+        if (quoteIndex < 0) {
+          quoteIndex = 0;
+        }
+        quotes.forEach((quote) => {
+          quote.classList.remove("active");
+        });
+        for (let i = quoteIndex; i < quoteIndex + perQuotes; i++) {
+          quotes[i].classList.add("active");
+        }
+      });
+      quoteNext.addEventListener("click", () => {
+        quoteIndex += perQuotes;
+        if (quoteIndex > quotes.length - 1) {
+          quoteIndex = quotes.length - 1;
+        }
+        quotes.forEach((quote) => {
+          quote.classList.remove("active");
+        });
+        for (let i = quoteIndex; i < quoteIndex + perQuotes; i++) {
+          quotes[i].classList.add("active");
+        }
+      });
     }
   })();
 });
